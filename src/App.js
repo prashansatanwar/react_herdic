846f9784eeff7470ecfbc23a61c9dc54b85d5ca2
tanmay-j <tanmay18271@iiitd.ac.in>
2020-07-24 11:51:05 +0530

    Navbar v1.0

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {Route, Switch} from 'react-router-dom';
 import HomePage from './components/HomePage'
 import SignupPage from './components/SignupPage';
 import Dashboard from './components/Dashboard';
+import Navigation from './components/Navigation'
+
 
 function App() {
   return (
-    <div className="mt-5" id="app">
-
+    <div id="app">
+      <Navigation   />
       <Switch>
         <Route path = "/" component = {HomePage} exact />
         <Route path = "/Signup" component = {SignupPage} />
---
+++ src/components/Navigation.js
@@ -0,0 +1,42 @@
+import React,{Component} from 'react';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem }  from 'reactstrap';
+import './Navigation.css';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fa } from '@fortawesome/free-solid-svg-icons'
+
+class Navigation extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            navOpen:false
+        };
+    }
+
+    toggleNav = () => {
+        this.setState({navOpen:!this.state.navOpen});
+        console.log(this.state);
+    };
+
+    render(){
+        return(
+            <Navbar id="navigationBar" expand="md">
+                <NavbarBrand>Herdic</NavbarBrand>
+                <NavbarToggler onClick={this.toggleNav} className="ml-auto"> Menu</NavbarToggler>
+                <Collapse isOpen={this.state.navOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem className="pr-3">
+                            <NavLink className="text-white" to="/">Log in</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="text-white" to="/signUp">Sign Up</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        );
+    }
+}
+
+export default Navigation;
